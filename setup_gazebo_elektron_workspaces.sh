094df837535f8fc42e5e20453e879a419ddf6ebc
dudekw <wojciechsbox@gmail.com>
2018-11-23 10:50:44 +0100

    Create setup_gazebo_elektron_workspaces.sh
    script to compile gazebo and elektron workaspaces and install them to /opt/

---
+++ setup_gazebo_elektron_workspaces.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#compile gazebo and elektron workspaces in a fakeroot environment
+git clone https://github.com/RCPRG-ros-pkg/RCPRG_rosinstall
+cd RCPRG_rosinstall
+chmod +x setup.sh
+./setup.sh -g -e -F build -b Release -i /opt
+
+# coppy compiled workspaces to your /opt directory
+sudo cp -r build/opt/ws_gazebo /opt/
+sudo cp -r build/opt/ws_elektron /opt/
