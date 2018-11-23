75661e908bb970ae4f7a02a2f1c02f0a64d455a7
dudekw <wojciechsbox@gmail.com>
2018-11-23 11:59:18 +0100

    Update setup_elektron_only_workspace.sh
    if branch 'melodic-setup-working' exists, use it

--- setup_elektron_only_workspace.sh
+++ setup_elektron_only_workspace.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 #compile elektron workspaces in a fakeroot environment
-git clone https://github.com/RCPRG-ros-pkg/RCPRG_rosinstall
+branch_name="melodic-setup-working"
+if [ `git branch --list $branch_name` ]
+then
+    git clone --single-branch -b $branch_name https://github.com/RCPRG-ros-pkg/RCPRG_rosinstall
+fi
+else
+    git clone https://github.com/RCPRG-ros-pkg/RCPRG_rosinstall
+fi
 cd RCPRG_rosinstall
 chmod +x setup.sh
 ./setup.sh -e -F -d build -b Release -i /opt
