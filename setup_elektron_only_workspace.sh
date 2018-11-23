017a6f0087ea6d41e4787c93b485668bf40f5e3c
dudekw <wojciechsbox@gmail.com>
2018-11-23 11:47:02 +0100

    Update setup_elektron_only_workspace.sh
    adds file to compile from source the elektron workspace and install it to /opt directory

--- setup_elektron_only_workspace.sh
+++ setup_elektron_only_workspace.sh
@@ -4,7 +4,7 @@
 git clone https://github.com/RCPRG-ros-pkg/RCPRG_rosinstall
 cd RCPRG_rosinstall
 chmod +x setup.sh
-./setup.sh -e -F build -b Release -i /opt
+./setup.sh -e -F -d build -b Release -i /opt
 
 # coppy compiled elektron workspace to your /opt directory
 sudo cp -r build/opt/ws_elektron /opt/
