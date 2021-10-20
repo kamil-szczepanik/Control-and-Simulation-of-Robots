368b964509cca50e89908fefc75e63830bbb52cf
kamil-szczepanik <kamil.szczepanik00@gmail.com>
2021-10-21 00:46:46 +0200

    zad1 modifications: fixed rospy.Time

--- scripts/zad1.py
+++ scripts/zad1.py
@@ -13,7 +13,7 @@ def zad1():
     VEL_LIN = 0.08
     VEL_ANG = 0.09
 
-    TURN_RIGHT = 0
+    TURN_RIGHT = 1
     SIDE_LENGHT = 1.0
 
     pub = rospy.Publisher('key_vel', Twist, queue_size=10)
@@ -22,22 +22,29 @@ def zad1():
     rate = rospy.Rate(50)  # 10hz
 
     vel = Twist()
-    START_TIME = rospy.Time.now()
-    global start_time
-    start_time = time.time()
 
+    i = 0
     while not rospy.is_shutdown():
+        i+=1
+        global time_now
+        if i>2:
+          
+            now = rospy.Time.now()
+            time_now = now.secs + float(now.nsecs)/1000000000
+            lin, ang = go_square(SIDE_LENGHT, TURN_RIGHT, VEL_LIN, VEL_ANG)
+            vel.linear.x = lin
+            vel.angular.z = ang
 
-        lin, ang = go_square(SIDE_LENGHT, TURN_RIGHT, VEL_LIN, VEL_ANG)
-        vel.linear.x = lin
-        vel.angular.z = ang
+        if i == 2:
+            global start_time
+            now = rospy.Time.now()
+            start_time = now.secs + float(now.nsecs)/1000000000
+        
         pub.publish(vel)
-
         rate.sleep()
 
 
 def callback(data):
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.pose.pose.position.x)
     global pose
     pose = Pose()
     pose = data.pose.pose
@@ -52,24 +59,27 @@ def calc_time_ang(vel_ang):
 
 
 def go_square(side_lenght, turn_right, vel_lin, vel_ang):
-
     global start_time
 
     TIME_LIN = calc_time_lin(side_lenght, vel_lin)
     TIME_ANG = calc_time_ang(vel_ang)
 
-    duration = time.time() - start_time
+    duration = time_now - start_time
     print(duration)
     if duration < TIME_LIN:
         lin = vel_lin
         ang = 0
     elif duration >= TIME_LIN and duration <= TIME_LIN + TIME_ANG:
         lin = 0.0
-        ang = vel_ang
+        if turn_right:
+            ang = -vel_ang
+        else:
+            ang = vel_ang
     else:
-        start_time = time.time()
+        start_time = time_now
         lin = 0
-        ang = vel_ang
+        ang = 0
+
 
     return lin, ang
 
